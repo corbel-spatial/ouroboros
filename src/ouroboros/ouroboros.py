4f411b40b594e7292d86fd64ec1b5765d25ee822
p-vdp <peter@corbelspatial.com>
2025-07-15 11:43:25 -0700

    rewrite in progress, refactor done

---
+++ src/ouroboros/ouroboros.py
@@ -0,0 +1,361 @@
+import os
+from collections.abc import MutableMapping, MutableSequence
+from typing import Iterator
+import pathlib
+from unittest import case
+
+import fiona
+import geopandas as gpd
+import numpy as np
+import pandas as pd
+from pyogrio.errors import DataSourceError
+
+
+class FeatureClass(MutableSequence):  # TODO add geometry attribute and enforce; use feature datasets
+    """
+    Wraps a GeoDataFrame, and allows access like an arcpy.da cursor in memory.
+    Must use 'save' method to write to disk.
+    """
+
+    def __init__(
+        self,
+        fc_name: str,
+        src: None | os.PathLike | str | gpd.GeoDataFrame = None,
+        feature_dataset: None | str = None,
+    ):
+        self.feature_dataset = feature_dataset
+        self.gdb_path = None
+        self.path = None
+        self.saved = False
+
+        # parse fc_name
+        if not isinstance(fc_name, str):
+            raise TypeError("fc_name must be a string")
+        if fc_name[0].isdigit():
+            fc_name = (
+                "_" + fc_name
+            )  # fc name cannot start with a digit, mimicking ArcGIS Pro renaming behavior
+        self.name = fc_name
+
+        # parse src
+        if isinstance(src, gpd.GeoDataFrame):
+            self._data = src
+        elif isinstance(src, os.PathLike) or isinstance(src, str):
+            if (
+                not os.path.exists(src)
+                or not os.path.isdir(src)
+                or not src.endswith(".gdb")
+            ):
+                raise FileNotFoundError(src)
+            else:
+                self.gdb_path = pathlib.Path(os.path.abspath(src))
+                self._data: gpd.GeoDataFrame = fc_to_gdf(self.gdb_path, self.name)
+                self.saved = True
+        else:
+            self._data = gpd.GeoDataFrame()
+
+        # parse path
+        if self.gdb_path:
+            if self.feature_dataset:
+                self.path = os.path.join(self.gdb_path, self.feature_dataset, self.name)
+            else:
+                self.path = os.path.join(self.gdb_path, self.name)
+        else:
+            self.path = self.name
+
+    def __delitem__(self, index):
+        if not isinstance(index, int):
+            raise TypeError("index must be an integer")
+        self._data = pd.concat(
+            [self._data.iloc[: index - 1], self._data.iloc[index:]]
+        ).reset_index(drop=True)
+        self.saved = False
+
+    def __getitem__(self, index: int) -> gpd.GeoDataFrame:
+        if not isinstance(index, int):
+            raise TypeError("index must be an integer")
+        return self._data.iloc[[index]]
+
+    def __iter__(self):
+        return self._data.itertuples()
+
+    def __len__(self):
+        if len(self._data.shape) != 2:
+            raise ValueError("GeoDataFrame must be two-dimensional")
+        return self._data.shape[0]
+
+    def __repr__(self):
+        return (
+            f"ob.FeatureClass('{self.name}', gpd.GeoDataFrame({self._data.to_json()}))"
+        )
+
+    def __setitem__(
+        self,
+        index: tuple[int, int | str],
+        value,
+    ):
+        """index is a tuple (row, column) where column can be int or str"""
+        row, column = index
+
+        if not isinstance(row, int):
+            raise TypeError("row index must be an integer")
+        if not isinstance(column, int) and not isinstance(column, str):
+            raise TypeError("column index must be an integer or a column name string")
+
+        if type(column) is int:
+            self._data.iat[row, column] = value
+        else:
+            self._data.at[row, column] = value
+        self.saved = False
+
+    def __str__(self):
+        return self.path
+
+    def append(self, value):
+        self.insert(-1, value)
+
+    def clear(self):
+        self._data = self._data[0:0]
+
+    def count(self, value):  # TODO
+        raise NotImplementedError
+
+    def describe(self):
+        return {
+            "feature_dataset": self.feature_dataset,
+            "fields": self.get_fields(),
+            "gdb_path": str(self.gdb_path),
+            "name": self.name,
+            "path": str(self.path),
+            "row_count": len(self._data),
+            "saved": self.saved,
+        }
+
+    def get_fields(self):
+        return self._data.columns.to_list()
+
+    def head(self, n: int = 10, silent: bool = False) -> gpd.GeoDataFrame:
+        h = self._data.head(n)
+        if not silent:
+            print(h)
+        return h
+
+    def index(self, oid, start=0, stop=None):  # TODO
+        raise NotImplementedError
+
+    def insert(self, index: int, value: gpd.GeoDataFrame):
+        if not isinstance(index, int):
+            raise TypeError("Index must be an integer")
+        if not isinstance(value, gpd.GeoDataFrame):
+            raise TypeError("Value must be an instance of gpd.GeoDataFrame")
+        if not (value.columns == self._data.columns).all():
+            raise ValueError("Schemas must match")
+
+        match index:
+            case 0:
+                c = [value, self._data]
+            case -1:
+                c = [self._data, value]
+            case _:
+                c = [self._data.iloc[:index], value, self._data.iloc[index:]]
+        self._data = pd.concat(c, ignore_index=True)
+        self.saved = False
+
+    def save(
+        self,
+    ):
+        """Save to disk"""
+        if not self.gdb_path:
+            raise AttributeError("gdb_path not set")
+        else:
+            gdf_to_fc(
+                self._data,
+                self.gdb_path,
+                self.name,
+                self.feature_dataset,
+                overwrite=True,
+            )
+            self.saved = True
+
+    def sort(
+        self,
+        field_name: str,
+        ascending: bool = True,
+    ):
+        self._data.sort_values(by=field_name, ascending=ascending, inplace=True)
+
+
+class GeoDatabase(MutableMapping):
+    """Dict of ouroboros.FeatureClass objects, loaded into memory"""
+
+    def __init__(self, gdb_path: os.PathLike | str):
+        if not os.path.exists(gdb_path):
+            raise FileNotFoundError(gdb_path)
+
+        self.path = os.path.abspath(gdb_path)
+        self._data = dict()
+        self.saved = False
+        self.reload()
+
+    def __delitem__(self, fc_name: str, /):
+        if not isinstance(fc_name, str):
+            raise TypeError("Feature class name must be a string")
+        if fc_name in self._data:
+            del self._data[fc_name]
+            self.saved = False
+
+    def __getitem__(self, fc_name: str, /) -> FeatureClass:
+        if not isinstance(fc_name, str):
+            raise TypeError("Feature class name must be a string")
+        return self._data.get(fc_name)
+
+    def __iter__(self) -> Iterator[FeatureClass]:
+        return iter(self._data.values())
+
+    def __len__(self) -> int:
+        return len(self._data)
+
+    def __setitem__(self, fc_name: str, fc: FeatureClass, /):
+        if not isinstance(fc_name, str):
+            raise TypeError("Feature class name must be a string")
+        if not isinstance(fc, FeatureClass):
+            raise TypeError("Input must be an ouroboros.FeatureClass object")
+
+        if fc_name in self._data:
+            raise KeyError(f"Feature class '{fc_name}' already exists")
+        else:
+            self._data[fc_name] = fc
+            self._data[fc_name].name = fc_name
+            self.saved = False
+
+    def reload(self):
+        """Load from disk, overwriting data in memory"""
+        self._data = dict()
+        gdb_fcs = list_fcs(self.path)
+        for fc_name in gdb_fcs:
+            self._data[fc_name] = FeatureClass(fc_name, self.path)
+        self.saved = True
+
+    def save(self):
+        """
+        Save in-memory object data to disk
+        """
+        # Delete fcs that are not in _data
+        gdb_fcs = list_fcs(self.path)
+        for fc_name in gdb_fcs:
+            if fc_name not in self._data:
+                delete_fc(self.path, fc_name)
+
+        # Add or overwrite feature classes on disk from _data
+        # TODO feature dataset support
+        for fc_name, fc in self._data.items():
+            # noinspection PyProtectedMember
+            gdf_to_fc(fc._data, self.path, fc_name, overwrite=True)
+        self.saved = True
+
+
+def delete_fc(
+    gdb_path: os.PathLike | str,
+    fc_name: str,
+) -> bool:
+    if not os.path.exists(gdb_path):
+        raise FileNotFoundError(gdb_path)
+
+    if not isinstance(fc_name, str):
+        raise TypeError("Feature class name must be a string")
+
+    if fc_name in list_fcs(gdb_path):
+        fiona.remove(gdb_path, "OpenFileGDB", fc_name)
+        return True
+    else:
+        return False
+
+
+def fc_to_gdf(
+    gdb_path: os.PathLike | str,
+    fc_name: str,
+) -> gpd.GeoDataFrame:
+    """Feature Class to GeoDataFrame"""
+    if not os.path.exists(gdb_path):
+        raise FileNotFoundError(gdb_path)
+
+    if not isinstance(fc_name, str):
+        raise TypeError("Feature class name must be a string")
+    gdf: gpd.GeoDataFrame = gpd.read_file(gdb_path, layer=fc_name, driver="OpenFileGDB")
+    gdf = gdf.rename_axis("ObjectID")  # use ObjectID as dataframe index
+    return gdf
+
+
+def gdf_to_fc(
+    gdf: gpd.GeoDataFrame,
+    gdb_path: os.PathLike | str,
+    fc_name: str,
+    feature_dataset: str = None,
+    overwrite: bool = False,
+    compatibility: bool = True,
+    reindex: bool = False,
+):
+    """
+    GeoDataFrame to Feature Class
+
+    References:
+    - https://geopandas.org/en/stable/docs/reference/api/geopandas.GeoDataFrame.to_file.html
+    - https://gdal.org/en/stable/drivers/vector/openfilegdb.html
+    """
+    if not isinstance(gdf, gpd.GeoDataFrame):
+        if isinstance(gdf, gpd.GeoSeries):
+            gdf = gpd.GeoDataFrame(gdf)
+        else:
+            raise TypeError("Input must be a GeoDataFrame or GeoSeries")
+
+    if not isinstance(fc_name, str):
+        raise TypeError("Feature class name must be a string")
+
+    if feature_dataset and not isinstance(feature_dataset, str):
+        raise TypeError("Feature dataset name must be a string")
+
+    for param in (overwrite, compatibility, reindex):
+        if not isinstance(param, bool):
+            raise TypeError(f"Parameter {param} must be a boolean")
+
+    layer_options = {
+        "TARGET_ARCGIS_VERSION": True if compatibility else False,
+        "OPENFILEGDB_IN_MEMORY_SPI": True if reindex else False,
+        "FEATURE_DATASET": feature_dataset,
+    }
+
+    if os.path.exists(gdb_path):
+        if fc_name in list_fcs(gdb_path):
+            if overwrite:
+                delete_fc(gdb_path, fc_name)
+            else:
+                raise FileExistsError(
+                    f"{fc_name} already exists. To overwrite it use: gdf_to_fc(gdf, gdb_path, fc_name, overwrite=True"
+                )
+
+    # convert dataframe index back to ObjectID
+    if "ObjectID" not in gdf.columns:
+        gdf = gdf.rename_axis("ObjectID")
+    gdf.reset_index(inplace=True)
+    gdf["ObjectID"] = gdf["ObjectID"].astype(np.int32)
+    gdf["ObjectID"] = gdf["ObjectID"] + 1
+
+    gdf.to_file(
+        gdb_path,
+        driver="OpenFileGDB",
+        layer=fc_name,
+        layer_options=layer_options,
+    )
+
+
+def list_fcs(gdb_path: os.PathLike | str) -> list:
+    """
+    List feature classes in geodatabase
+    """
+    if not os.path.exists(gdb_path):
+        raise FileNotFoundError(gdb_path)
+
+    try:
+        return gpd.list_layers(gdb_path)["name"].values.tolist()
+    except DataSourceError:  # empty GeoDatabase
+        return list()
--- tests/tests.py
+++ tests/tests.py
@@ -1,25 +1,30 @@
 import os
-import pathlib
-from typing import NamedTuple
+import uuid
 from random import uniform
 
-import pytest
 import geopandas as gpd
 import pandas as pd
+import pytest
 from shapely.geometry import Point
 
 from src import ouroboros as ob
+
 
 SAMPLE_DEFS = ("test_points", "test_lines", "test_polygons")
 SAMPLES = 1000
 
 
 @pytest.fixture
-def test_gdb(tmp_path):
-    test_gdb_path = pathlib.Path(os.path.join(tmp_path, "test.gdb"))
+def gdb(tmp_path):
+    test_gdb_path = os.path.join(tmp_path, "test.gdb")
 
     test_points = [Point(uniform(-170, 170), uniform(-70, 70)) for i in range(SAMPLES)]
-    test_points = gpd.GeoDataFrame(crs="EPSG:4326", geometry=test_points)
+    test_fields = {
+        "sample1": [uuid.uuid4() for i in range(SAMPLES)],
+        "sample2": [uuid.uuid4() for i in range(SAMPLES)],
+        "sample3": [uuid.uuid4() for i in range(SAMPLES)],
+    }
+    test_points = gpd.GeoDataFrame(test_fields, crs="EPSG:4326", geometry=test_points)
     ob.gdf_to_fc(
         test_points,
         test_gdb_path,
@@ -49,109 +54,195 @@ def test_gdb(tmp_path):
     return ob.GeoDatabase(test_gdb_path)
 
 
-def test_create_gdb_fixture(test_gdb):
-    assert isinstance(test_gdb, ob.GeoDatabase)
+def test_create_gdb_fixture(gdb):
+    assert isinstance(gdb, ob.GeoDatabase)
 
 
-def fc_to_gdf(test_gdb):
-    raise NotImplementedError
+class TestUtilityFunctions:
+    def test_delete_fc(self, gdb):
+        fcs = ob.list_fcs(gdb.path)
+        count = len(fcs)
+        for fc_name in fcs:
+            ob.delete_fc(gdb.path, fc_name)
+            gdb.reload()
+            assert len(gdb) < count
+            count -= 1
+        assert len(gdb) == 0
 
+    def test_fc_to_gdf(self, gdb):
+        for fc in gdb:
+            gdf = ob.fc_to_gdf(gdb.path, fc.name)
+            assert isinstance(gdf, gpd.GeoDataFrame)
 
-def gdf_to_fc(test_gdb):
-    raise NotImplementedError
+    def test_gdf_to_fc(self, gdb):
+        for fc in gdb:
+            gdf = fc._data
+            ob.gdf_to_fc(gdf, gdb.path, fc.name + "_copy")
+        gdb.reload()
+        assert len(gdb) == 6
+
+    def test_list_fcs(self, gdb):
+        assert len(ob.list_fcs(gdb.path)) == 3
 
 
 class TestFeatureClass:
-    def test_instantiate(self, test_gdb):
-        print(test_gdb.path)
-        for name in test_gdb.names:
-            print(name)
-            fc_obj = ob.FeatureClass(test_gdb.path, name)
+    def test_instantiate_gdb(self, gdb):
+        for fc in gdb:
+            fc_obj = ob.FeatureClass(
+                fc.name,
+                gdb.path,
+            )
             assert isinstance(fc_obj, ob.FeatureClass)
+            assert fc_obj.saved is True
+            assert str(fc) == fc.path
 
-    def test_delitem(self, test_gdb):
-        for fc in test_gdb:
-            del test_gdb[fc][500]
-            assert len(test_gdb[fc]) == SAMPLES - 1
+    def test_instatiate_gdf(self):
+        fc = ob.FeatureClass("test", gpd.GeoDataFrame(geometry=[Point(0, 1)]))
+        assert fc.saved is False
+        assert str(fc) == fc.name
 
-    def test_getitem(self, test_gdb):
-        for fc in test_gdb:
-            assert isinstance(test_gdb[fc][0], pd.Series)
-            assert isinstance(test_gdb[fc][-1], pd.Series)
+    def test_instatiate_none(self):
+        fc = ob.FeatureClass("test")
+        assert fc.saved is False
+        assert str(fc) == fc.name
 
-    def test_iter(self, test_gdb):
-        for fc in test_gdb:
-            for row in test_gdb[fc]:
+    def test_delitem(self, gdb):
+        for fc in gdb:
+            del fc[500]
+            assert len(fc) == SAMPLES - 1
+
+    def test_getitem(self, gdb):
+        for fc in gdb:
+            assert isinstance(fc[0], gpd.GeoDataFrame)
+            assert isinstance(fc[-1], gpd.GeoDataFrame)
+
+    def test_iter(self, gdb):
+        for fc in gdb:
+            for row in fc:
                 assert isinstance(row, tuple)
                 break
 
-    def test_len(self, test_gdb):
-        for fc in test_gdb:
-            assert len(test_gdb[fc]) == SAMPLES
+    def test_len(self, gdb):
+        for fc in gdb:
+            assert len(fc) == SAMPLES
 
-    def test_setitem(self, test_gdb):
-        for fc in test_gdb:
-            test_gdb[fc][(0, "geometry")] = None
-            test_gdb[fc][(-1, 0)] = None
+    def test_repr(self, gdb):
+        for fc in gdb:
+            assert isinstance(repr(fc), str)
+            assert isinstance(eval(repr(fc)), ob.FeatureClass)
 
-    def test_copy(self, test_gdb):
-        count = len(test_gdb)
-        for fc in test_gdb:
-            with pytest.raises(KeyError):
-                test_gdb[fc] = test_gdb[fc]
-            test_gdb[fc].copy(fc + "_copy")
-        test_gdb.reload()  # TODO auto reload?
-        assert len(test_gdb) == count * 2
+    def test_setitem(self, gdb):
+        for fc in gdb:
+            fc[(0, "geometry")] = None
+            fc[(-1, 0)] = None
 
-    def test_insert(self, test_gdb):
-        for fc in test_gdb:
-            new_row = test_gdb[fc][500]
-            test_gdb[fc].insert(600, new_row)
-            assert len(test_gdb[fc]) == SAMPLES + 1
+    def test_str(self, gdb):
+        for fc in gdb:
+            assert str(fc) == fc.path
+            print(str(fc))
 
-    def test_save(self, test_gdb):
-        for fc in test_gdb:
-            test_gdb[fc].save()
-            assert test_gdb[fc].saved is True
+    def test_append(self, gdb):
+        fc = gdb["test_points"]
+        count = len(fc)
+        new_row = fc[0]
+        fc.append(new_row)
+        assert len(fc) == count + 1
+        assert fc[0].iat[0, 0] == fc[-1].iat[0, 0]
+
+    def test_clear(self, gdb):
+        for fc in gdb:
+            fc.clear()
+            assert len(fc) == 0
+
+    def test_count(self, gdb):
+        raise NotImplementedError
+
+    def test_describe(self, gdb):
+        for fc in gdb:
+            assert isinstance(fc.describe(), dict)
+            print(fc.describe())
+
+    def test_get_fields(self, gdb):
+        for fc in gdb:
+            print(fc.get_fields())
+
+    def test_head(self, gdb):
+        for fc in gdb:
+            h = fc.head(5)
+            assert isinstance(h, pd.DataFrame)
+            assert len(h) == 5
+
+    def test_insert(self, gdb):
+        fc = gdb["test_points"]
+        new_row = fc[500]
+        fc.insert(600, new_row)
+        assert len(fc) == SAMPLES + 1
+        assert fc[500].iat[0, 0] == fc[600].iat[0, 0]
+
+    def test_save(self, gdb):
+        for fc in gdb:
+            fc.save()
+            assert fc.saved is True
+
+    def test_sort(self, gdb):
+        fc = gdb["test_points"]
+        case1 = fc[0].iat[0, 0]
+        fc.sort("sample1", ascending=True)
+        fc.head()
+        case2 = fc[0].iat[0, 0]
+        fc.sort("sample1", ascending=False)
+        case3 = fc[0].iat[0, 0]
+        assert case1 != case2 != case3
+        fc.head()
 
 
 class TestGeoDatabase:
-    def test_instantiate(self, test_gdb):
-        assert isinstance(test_gdb, ob.GeoDatabase)
+    def test_instantiate(self, gdb):
+        assert isinstance(gdb, ob.GeoDatabase)
 
-    def test_delitem(self, test_gdb):
-        count = len(test_gdb)
-        for fc in test_gdb:
-            del test_gdb[fc]
-            assert len(test_gdb) < count
+    def test_delitem(self, gdb):
+        fcs = ob.list_fcs(gdb.path)
+        count = len(fcs)
+        for fc_name in fcs:
+            del gdb[fc_name]
+            assert len(gdb) < count
             count -= 1
-        assert len(test_gdb) == 0
+        assert len(gdb) == 0
 
-    def test_getitem(self, test_gdb):
-        for fc in test_gdb:
-            assert isinstance(test_gdb[fc], ob.FeatureClass)
-
-    def test_iter(self, test_gdb):
-        count = 0
-        for name, fc in test_gdb.items():
-            count += 1
-            assert isinstance(name, str)
+    def test_getitem(self, gdb):
+        for fc in gdb:
             assert isinstance(fc, ob.FeatureClass)
-        assert count == len(test_gdb)
 
-    def test_len(self, test_gdb):
-        assert len(test_gdb) == 3
+    def test_iter(self, gdb):
+        count = 0
+        for fc in gdb:
+            count += 1
+            assert isinstance(fc.name, str)
+            assert isinstance(fc, ob.FeatureClass)
+        assert count == len(gdb)
 
-    def test_setitem(self, test_gdb):
-        count = len(test_gdb)
-        for fc in test_gdb:
+    def test_len(self, gdb):
+        assert len(gdb) == 3
+
+    def test_setitem(self, gdb):
+        fcs = ob.list_fcs(gdb.path)
+        count = len(fcs)
+        for fc_name in fcs:
             with pytest.raises(KeyError):
-                test_gdb[fc] = test_gdb[fc]
-            test_gdb[fc].copy(fc + "_copy")
-        test_gdb.reload()  # TODO auto reload?
-        assert len(test_gdb) == count * 2
+                gdb[fc_name] = gdb[fc_name]
+            gdb[fc_name + "_copy"] = gdb[fc_name]
+            assert gdb[fc_name + "_copy"].name == fc_name + "_copy"
+        assert len(gdb) == count * 2
 
-    def test_reload(self, test_gdb):
-        count = len(test_gdb)
-        test_gdb.reload()
-        assert len(test_gdb) == count
+    def test_reload(self, gdb):
+        for fc in gdb:
+            ob.gdf_to_fc(fc._data, gdb.path, fc.name + "_copy")
+        assert len(gdb) == 3
+        gdb.reload()
+        assert len(gdb) == 6
+
+    def test_save(self, gdb):
+        for fc in gdb:
+            ob.gdf_to_fc(fc._data, gdb.path, fc.name + "_copy")
+        gdb.save()
+        assert gdb.saved is True
