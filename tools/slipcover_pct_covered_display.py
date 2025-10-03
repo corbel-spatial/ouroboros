3a5f4cb01cff11d7cbf7a6818657a9ca0813dfc8
p-vdp <peter@corbelspatial.com>
2025-10-03 14:16:03 -0700

    ruff format everything

--- tools/slipcover_pct_covered_display.py
+++ tools/slipcover_pct_covered_display.py
@@ -8,9 +8,9 @@ cov_file = argv[1]
 
 with open(cov_file, "r") as f:
     data = json.load(f)
-pct = data['summary']['percent_covered']
+pct = data["summary"]["percent_covered"]
 pct_display = f"{int(round(pct, 0))}%"
-data['summary']['percent_covered_display'] = pct_display
+data["summary"]["percent_covered_display"] = pct_display
 with open(cov_file, "w") as f:
     json.dump(data, f, indent=4)
 print(pct_display)
